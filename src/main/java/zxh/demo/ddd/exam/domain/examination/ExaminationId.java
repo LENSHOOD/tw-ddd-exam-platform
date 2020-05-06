726f7f7e4ddb1d4906eb7cb58044649ba1374ffa
zhangxuhai <xuhai.zhang@thoughtworks.com>
2020-05-07 00:25:50 +0800

    feat: provide examintion

---
+++ src/main/java/zxh/demo/ddd/exam/domain/examination/ExaminationId.java
@@ -0,0 +1,13 @@
+package zxh.demo.ddd.exam.domain.examination;
+
+import lombok.Data;
+
+/**
+ * ExaminationId:
+ * @author zhangxuhai
+ * @date 2020/5/6
+*/
+@Data
+public class ExaminationId {
+    private String id;
+}
