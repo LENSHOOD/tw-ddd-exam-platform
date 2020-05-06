0e98d27cf03e49f272b9c09c52edbcc1d12b9699
zhangxuhai <xuhai.zhang@thoughtworks.com>
2020-05-06 22:30:40 +0800

    feat: provide AnswerSheet

---
+++ src/main/java/zxh/demo/ddd/exam/domain/answersheet/AnswerSheetId.java
@@ -0,0 +1,13 @@
+package zxh.demo.ddd.exam.domain.answersheet;
+
+import lombok.Data;
+
+/**
+ * AnswerSheetId:
+ * @author zhangxuhai
+ * @date 2020/5/6
+*/
+@Data
+public class AnswerSheetId {
+    private String id;
+}
--- src/main/java/zxh/demo/ddd/exam/domain/paper/PaperId.java
+++ src/main/java/zxh/demo/ddd/exam/domain/paper/PaperId.java
@@ -1,10 +1,13 @@
 package zxh.demo.ddd.exam.domain.paper;
 
+import lombok.Data;
+
 /**
  * PaperId:
  * @author zhangxuhai
  * @date 2020/4/17
 */
+@Data
 public class PaperId {
     private String id;
 }
